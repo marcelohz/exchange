f4335523765595b7fb53b3a396e91e2dbf26022d
Marcelo Zamperetti <marcelohz@gmail.com>
2023-10-17 08:51:03 -0300

    removed dead code

--- src/main/java/dev/exchange/service/ExceededMaxCoinsException.java
+++ src/main/java/dev/exchange/service/ExceededMaxCoinsException.java
@@ -2,8 +2,6 @@ package dev.exchange.service;
 
 public class ExceededMaxCoinsException extends RuntimeException {
 
-    public static String errorCode = "MAX_COINS_EXCEEDED";
-
     public ExceededMaxCoinsException(String message) {
         super(message);
     }
--- src/main/java/dev/exchange/service/InsufficientCoinsException.java
+++ src/main/java/dev/exchange/service/InsufficientCoinsException.java
@@ -2,8 +2,6 @@ package dev.exchange.service;
 
 public class InsufficientCoinsException extends RuntimeException {
 
-    public static String errorCode = "INSUFFICIENT_COINS";
-
     public InsufficientCoinsException(String message) {
         super(message);
     }
